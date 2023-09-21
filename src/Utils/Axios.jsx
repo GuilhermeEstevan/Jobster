d30710951a354b28a991f75421793683cac78bcd
GuilhermeEstevan <guilhermeestevangui@hotmail.com>
2023-09-21 11:24:40 -0300

    url

--- src/Utils/Axios.jsx
+++ src/Utils/Axios.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 // })
 
 const customFetch = axios.create({
-    baseURL: 'https://temp-jobs-api-ogq6.onrender.com/api/v1'
+    baseURL: 'https://temp-jobs-api-ogq6.onrender.com'
 })
 
 
